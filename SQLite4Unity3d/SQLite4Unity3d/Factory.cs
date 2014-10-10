b9d593f886a186a14001b6aeae3a2ce8f82e4942
Roberto Huertas <code@codecoding.com>
2014-10-10 13:12:26 +0200

    Firs upload

---
+++ SQLite4Unity3d/SQLite4Unity3d/Factory.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SQLite4Unity3d
+{
+    public class Factory : ISQLiteConnectionFactory
+    {
+        public ISQLiteConnection Create(string address)
+        {
+            return new SQLiteConnection(address, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+        }
+    }
+}
---
+++ SQLite4Unity3d/SQLite4Unity3dWP8/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SQLite4Unity3dWP8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQLite4Unity3dWP8")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dff95d7b-03ad-4627-bbb8-38a2ead07aa0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
