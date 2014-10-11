910fa276b44b944ee1e38061cfef878d66c78bd5
Newton_W <egonsch@gmail.com>
2014-10-11 18:20:32 +0200

    Fixed example

--- Example/Assets/Scripts/DataService.cs
+++ Example/Assets/Scripts/DataService.cs
@@ -10,54 +10,43 @@ public class DataService  {
 
 	public DataService(string DatabaseName){
 
-		var factory = new Factory ();
-		var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
-		//http://docs.unity3d.com/Manual/StreamingAssets.html
+        var factory = new Factory();
 
-#if UNITY_ANDROID && !UNITY_EDITOR
-		
-		// check if file exists in Application.persistentDataPath
-		var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
-		
-		if (!File.Exists(filepath))
-		{
-			Debug.Log("Database not in Persistent path");
-			// if it doesn't ->
-			// open StreamingAssets directory and load the db ->
-			var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
-			while (!loadDb.isDone) { }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
-			// then save to Application.persistentDataPath
-			File.WriteAllBytes(filepath, loadDb.bytes);
-			Debug.Log("Database written");
-		}
-		//open db connection
-		dbPath = filepath;
-		
-#elif UNITY_IOS && !UNITY_EDITOR
-		
-		// check if file exists in Application.persistentDataPath
-		var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
-		
-		if (!File.Exists(filepath))
-		{
-			Debug.Log("Database not in Persistent path");
-			// if it doesn't ->
-			// open StreamingAssets directory and load the db ->
-			var loadDb = Application.dataPath + "/Raw/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
-			
-			// then save to Application.persistentDataPath
-			File.Copy(loadDb, filepath);
-			Debug.Log("Database written");
-		}
-		//open db connection
-		dbPath = filepath;
-		
-		
-#elif !UNITY_ANDROID && !UNITY_WP8 && !UNITY_IOS
-		var path = Path.Combine(Directory.GetCurrentDirectory(), dbPath);
+#if !UNITY_EDITOR
+            var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
+#else
+        // check if file exists in Application.persistentDataPath
+        var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
+        if (!File.Exists(filepath))
+        {
+            Debug.Log("Database not in Persistent path");
+            // if it doesn't ->
+            // open StreamingAssets directory and load the db ->
+
+#if UNITY_ANDROID
+            var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
+            while (!loadDb.isDone) { }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
+            // then save to Application.persistentDataPath
+            File.WriteAllBytes(filepath, loadDb.bytes);
+#elif UNITY_IOS
+                 var loadDb = Application.dataPath + "/Raw/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
+                // then save to Application.persistentDataPath
+                File.Copy(loadDb, filepath);
+#elif UNITY_WP8
+                var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
+                // then save to Application.persistentDataPath
+                File.Copy(loadDb, filepath);
+           
 #endif
-		Debug.Log("Final PATH: " + dbPath);
-		_connection = factory.Create(dbPath);
+
+            Debug.Log("Database written");
+        }
+
+        var dbPath = filepath;
+#endif
+        _connection = factory.Create(dbPath);
+        Debug.Log("Final PATH: " + dbPath);     
+
 	}
 
 	public void CreateDB(){
--- SQLite4Unity3d/SQLite4Unity3dWP8/Factory.cs
+++ SQLite4Unity3d/SQLite4Unity3dWP8/Factory.cs
@@ -1,38 +1,10 @@
-﻿using System;
-using System.IO;
-using System.IO.IsolatedStorage;
-using System.Windows;
-
-namespace SQLite4Unity3d
+﻿namespace SQLite4Unity3d
 {
     public class Factory : ISQLiteConnectionFactory
     {
         public ISQLiteConnection Create(string address)
         {
-            var lastSlashPosition = address.LastIndexOf(@"/", StringComparison.InvariantCulture) + 1;
-            var filename = address.Substring(lastSlashPosition, address.Length - lastSlashPosition);
-
-            //let's check if it exists in our resources
-            var streamInfo = Application.GetResourceStream(new Uri(address, UriKind.Relative));
-            if (streamInfo == null)
-            {
-                return new SQLiteConnection(filename);
-            }
-
-            //let's copy data base from resources to isolatedstorage
-            using (var isf = IsolatedStorageFile.GetUserStoreForApplication())
-            {
-                if (isf.FileExists(filename))
-                {
-                    using (var fileStream = 
-                        new IsolatedStorageFileStream(filename, FileMode.Create, isf))
-                    {
-                        streamInfo.Stream.CopyTo(fileStream);
-                    }    
-                } 
-            }
-            
-            return new SQLiteConnection(filename);
+            return new SQLiteConnection(address, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
         }
     }
 }
